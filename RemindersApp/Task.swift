32574d4c8bd210e36f59aeb92f6b3bd6081b7298
Pratheeksha Ravindra Naik <prp412@udml18.cs.uregina.ca>
2018-10-26 19:29:44 -0600

    table view! table view table view

---
+++ RemindersApp/Task.swift
@@ -0,0 +1,34 @@
+//
+//  Task.swift
+//  RemindersApp
+//
+//  Created by Pratheeksha Ravindra Naik on 2018-10-26.
+//  Copyright © 2018. All rights reserved.
+//
+
+import UIKit
+
+class Task {
+    
+    //Mark: Properties
+    
+    var title: String
+    var photo: UIImage?
+    var currentDate: String
+    var dueDate: String
+    var priority: String
+    var notes: String
+    
+    //mark: initalization
+    
+    init(title: String, photo: UIImage?, currentDate: String, dueDate: String, priority: String, notes: String) {
+        self.currentDate = currentDate
+        self.dueDate = dueDate
+        self.notes = notes
+        self.photo = photo
+        self.title = title
+        self.priority = priority
+    }
+    
+    
+}
