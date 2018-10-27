64948da6970a723dd8087d03cd33e1dae98b2ab8
Pratheeksha Ravindra Naik <prp412@udml16.local>
2018-10-27 00:35:31 -0600

    Cancel works while existing an app

--- RemindersApp/TaskViewController.swift
+++ RemindersApp/TaskViewController.swift
@@ -32,7 +32,20 @@ class TaskViewController: UIViewController, UITextFieldDelegate, UIPickerViewDat
     
     
     @IBAction func cancel(_ sender: UIBarButtonItem) {
-        dismiss(animated: true, completion: nil)
+        
+        // Depending on style of presentation (modal or push presentation), this view controller needs to be dismissed in two different ways.
+        let isPresentingInAddMealMode = presentingViewController is UINavigationController
+        
+        if isPresentingInAddMealMode {
+            dismiss(animated: true, completion: nil)
+        }
+        else if let owningNavigationController = navigationController{
+            owningNavigationController.popViewController(animated: true)
+        }
+        else {
+            fatalError("The MealViewController is not inside a navigation controller.")
+        }
+        
     }
     
     
