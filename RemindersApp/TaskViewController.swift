32b7e3081d0827af081f0771644d728d6c80f26e
Pratheeksha Ravindra Naik <prp412@udml16.local>
2018-10-27 23:06:06 -0600

    Camera Works

--- RemindersApp/TaskViewController.swift
+++ RemindersApp/TaskViewController.swift
@@ -73,28 +73,35 @@ class TaskViewController: UIViewController, UITextFieldDelegate, UIPickerViewDat
     @IBAction func chooseImage(_ sender: Any) {
         let imagePickerController = UIImagePickerController();
         imagePickerController.delegate = self;
+        
         let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a source", preferredStyle: .actionSheet);
-        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(action : UIAlertAction) in
-            if UIImagePickerController.isSourceTypeAvailable(.camera){
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action : UIAlertAction) in
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
                 imagePickerController.sourceType = .camera
                 self.present(imagePickerController, animated: true, completion: nil)
+                
+            } else {
+                print("Cannot use camera")
             }
-            else{
-                print("Cannot use camera");
-            }
-            
-            
         }));
+        
         actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: {(action : UIAlertAction) in imagePickerController.sourceType = .photoLibrary
             self.present(imagePickerController, animated: true, completion: nil)
         }));
+        
         actionSheet.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil));
+        
         self.present(actionSheet, animated: true, completion: nil);
+        
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        picker.dismiss(animated: true, completion: nil);
+        picker.dismiss(animated: true, completion: nil)
     }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad();
         // Do any additional setup after loading the view, typically from a nib.
@@ -115,7 +122,7 @@ class TaskViewController: UIViewController, UITextFieldDelegate, UIPickerViewDat
             reminderImage.image = task.photo
         }
         
-        //Image Experimentation
+        //Image Experimentation - zooming and panning
         scrollView.delegate = self
         reminderImage.frame = CGRect(x: 0, y: 0, width: scrollView.frame.size.width, height: scrollView.frame.size.height)
         reminderImage.image = UIImage(named: "Image")
@@ -132,10 +139,18 @@ class TaskViewController: UIViewController, UITextFieldDelegate, UIPickerViewDat
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage;
-        reminderImage.image = image;
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        
+        reminderImage.image = image
         
         
+        
+        //camera
+        
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            reminderImage.contentMode = .scaleToFill
+            reminderImage.image = pickedImage
+        }
         //chnages start here..
         reminderImage.contentMode = UIView.ContentMode.center
         reminderImage.frame = CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: image.size.width, height: image.size.height))
